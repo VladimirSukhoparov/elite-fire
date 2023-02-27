47ded3dba8fe569701652853733c2f857afce431
VladimirSukhoparov <sukhoparov.v.n@gmail.com>
2023-02-27 18:14:39 +0500

    create app, build

---
+++ dist/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const e=document.querySelectorAll('input[type="range"]'),t=document.querySelector("#rangenumber");function n(e){let t=e.target;"range"!==e.target.type&&(t=document.getElementById("range"));const n=t.min,r=t.max,u=t.value;t.style.backgroundSize=100*(u-n)/(r-n)+"% 100%"}e.forEach((e=>{e.addEventListener("input",n)})),t.addEventListener("input",n)})();
